49d71e0999374e0b3c5a72427be31af204e3b025
0JojoMojo0 <148360842+0JojoMojo0@users.noreply.github.com>
2024-06-21 20:48:48 +0300

    testing away

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import Test from './Test';
 
 function App() {
    return (
@@ -7,6 +8,7 @@ function App() {
       <Navbar />
      <div className="content">
       <Home />
+      <Test />
      </div>
     </div>
   );
